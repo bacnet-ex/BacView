ee98303193db286a9a11726623ffc273d2ff6ae1
CI <ci@git.local>
2026-08-26 07:48:35 +0000

    Sync to GitHub

--- scripts/desktop_release.bat
+++ scripts/desktop_release.bat
@@ -3,4 +3,8 @@
 set MIX_ENV=prod
 set BACVIEW_DESKTOP=1
 
+REM Make sure first that the dependencies are present, otherwise phx.gen.secret fails
+mix deps.get
+FOR /F "tokens=*" %g IN ('mix phx.gen.secret') DO SET SECRET_KEY_BASE=%g
+
 mix desktop.setup && mix desktop.installer
--- scripts/desktop_release.sh
+++ scripts/desktop_release.sh
@@ -4,4 +4,8 @@ set -euo pipefail
 export MIX_ENV=prod
 export BACVIEW_DESKTOP=1
 
+# First make sure that the dependencies are present, otherwise phx.gen.secret fails
+mix deps.get
+export SECRET_KEY_BASE=$(mix phx.gen.secret)
+
 mix desktop.setup && mix desktop.installer
